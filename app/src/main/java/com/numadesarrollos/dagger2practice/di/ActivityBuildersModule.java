60448cc4b7a6676e813f92eba4ac5663fb45d1c1
Borja Angulo <bangulo@paradigmadigital.com>
2019-09-03 14:35:55 +0200

    Adding custom scopes

--- app/src/main/java/com/numadesarrollos/dagger2practice/di/ActivityBuildersModule.java
+++ app/src/main/java/com/numadesarrollos/dagger2practice/di/ActivityBuildersModule.java
@@ -1,9 +1,11 @@
 package com.numadesarrollos.dagger2practice.di;
 
 import com.numadesarrollos.dagger2practice.di.auth.AuthModule;
+import com.numadesarrollos.dagger2practice.di.auth.AuthScope;
 import com.numadesarrollos.dagger2practice.di.auth.AuthViewModelsModule;
 import com.numadesarrollos.dagger2practice.di.main.MainFragmentBuildersModule;
 import com.numadesarrollos.dagger2practice.di.main.MainModule;
+import com.numadesarrollos.dagger2practice.di.main.MainScope;
 import com.numadesarrollos.dagger2practice.di.main.MainViewModelsModule;
 import com.numadesarrollos.dagger2practice.ui.auth.AuthActivity;
 import com.numadesarrollos.dagger2practice.ui.main.MainActivity;
@@ -14,12 +16,14 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
+    @AuthScope
     @ContributesAndroidInjector(
         modules = {AuthViewModelsModule.class,
                 AuthModule.class}
     )
     abstract AuthActivity contributeAuthActivity();
 
+    @MainScope
     @ContributesAndroidInjector(
             modules = {MainFragmentBuildersModule.class,
                         MainViewModelsModule.class,
--- app/src/main/java/com/numadesarrollos/dagger2practice/di/main/MainModule.java
+++ app/src/main/java/com/numadesarrollos/dagger2practice/di/main/MainModule.java
@@ -15,21 +15,25 @@ import retrofit2.Retrofit;
 @Module
 public class MainModule {
 
+    @MainScope
     @Provides
     static MainApi provideMainApi(Retrofit retrofit){
         return retrofit.create(MainApi.class);
     }
 
+    @MainScope
     @Provides
     static PostsRecyclerAdapter provideAdapter(){
        return new PostsRecyclerAdapter();
     }
 
+    @MainScope
     @Provides
     static LinearLayoutManager provideLinearLayoutManager(Application application){
         return new LinearLayoutManager(application);
     }
 
+    @MainScope
     @Provides
     static VerticalSpaceItemDecoration provideItemDecoration(){
         return new VerticalSpaceItemDecoration(15);
